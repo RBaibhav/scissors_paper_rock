f682243b694d696a279daf4bbc06aaa0a701963b
R.Baibhav <r.baibhav2023@gmail.com>
2024-02-03 18:21:47 +0530

    function to caluculate score and decide winner

--- index.js
+++ index.js
@@ -6,41 +6,67 @@ function getComputerChoice() {
 
     return computerChoice;
 }
-
-
-let playerChoice = "paPer"
-const computerChoice = getComputerChoice();
-
-
-function game(playerChoice, computerChoice) {
+// decides who win or looses 
+function playRound(playerChoice, computerChoice) {
     let playerSelection = playerChoice.toLowerCase();  // converts the playerChoice to toLowerCase 
 
     if (playerSelection === "paper" && computerChoice === "rock") {
-        return "You win! paper chatches the rock";
+        return "win";
     }
     else if (playerSelection === computerChoice) {
-        return "Draw try again";
+        return "draw";
     }
     else if (playerSelection === "rock" && computerChoice === "paper") {
-        return "You Loose, Paper catches the rock";
+        return "lose";
     }
     else if (playerSelection === "rock" && computerChoice === "scissors") {
-        return "You Win! rock broke the scissors";
+        return "win";
     }
     else if (playerSelection === "scissors" && computerChoice === "rock") {
-        return "You Lose! rock broke the scissors";
+        return "lose";
     }
     else if (playerSelection === "paper" && computerChoice === "scissors") {
-        return "You Lose! scissors cuts the paper";
+        return "lose";
     }
     else if (playerSelection === "scissors" && computerChoice === "paper") {
-        return "You Win! scissors cuts the paper";
+        return "win";
     }
 
-    return "Enter the correct input";
+    return -1;
 }
 
-console.log(game(playerChoice, computerChoice))
 
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+// Will take prompt and calucalte the score for 5 games 
+    for (let i = 0; i < 5; i++) {
 
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Enter rock, paper or scissors");
+        console.log("you chose : ", playerChoice)
+        console.log("computer chose : ", computerChoice)
+
+        let result = playRound(playerChoice, computerChoice);
+        console.log(result)
+        if (result === "win") {
+            playerScore += 1;
+        } else if (result === "lose") {
+            computerScore += 1;
+        } else if (result === "draw") { }
+        else {
+            alert("Enter correct prompt")
+        }
+    }
+    // whill decide who won
+    if (playerScore > computerScore) {
+        console.log("You Win!!")
+    } else if (playerScore === computerScore) {
+        console.log("It's a draw");
+    } else {
+        console.log("You lose")
+    }
+}
+
+playGame();
 
