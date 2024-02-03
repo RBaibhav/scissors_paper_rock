2e902c0c3d1a3cef5b3e04c50c9a31499a3345f8
R.Baibhav <r.baibhav2023@gmail.com>
2024-02-03 16:37:13 +0530

    created a fucntion game and takes caseInsensitive inpuet

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
-    const randomnumber =  Math.floor(Math.random() * 3);
+    const randomnumber =  Math.floor(Math.random() * 3);    // Generate a random integer between 0 and 2 (inclusive)
     let computerChoice = choices[randomnumber];
 
     return computerChoice;
 }
 
-// Generate a random integer between 0 and 2 (inclusive)
+
+let playerChoice = "paPer"
+const computerChoice = getComputerChoice();
 
 
-console.log(getComputerChoice())
+function game(playerChoice, computerChoice) {
+    let playerSelection = playerChoice.toLowerCase();
 
+    if (playerSelection === "paper" && computerChoice === "rock") {
+        console.log("You win! paper chatches the rock")
+    } 
+    else if (playerSelection === computerChoice) {
+        console.log("Draw try again")
+    }
+    else if (playerSelection === "rock" && computerChoice === "paper") {
+        console.log("You Loose, Paper catches the rock")
+    }
+    else if (playerSelection === "rock" && computerChoice === "scissors") {
+        console.log("You Win! rock broke the scissors");
+    }
+    else if (playerSelection === "scissors" && computerChoice === "rock") {
+        console.log("You Lose! rock broke the scissors")
+    }
+    else if (playerSelection === "paper" && computerChoice === "scissors") {
+        console.log("You Lose! scissors cuts the paper");
+    }
+    else if (playerSelection === "scissors" && computerChoice === "paper") {
+        console.log("You Win! scissors cuts the paper");
+    }
+    else {
+        console.log("Enter the correct input")
+    }
+}
+
+game(playerChoice, computerChoice)
 
 
 
